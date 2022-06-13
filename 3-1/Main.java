2f0ea5c9ed3675c9f1210bc92c070d5127a8020e
Ota-Haruki <98738492+Ota-Haruki@users.noreply.github.com>
2022-06-13 17:25:46 +0900

    課題提出
    3章チェックテスト

--- 3-1/Main.java
+++ 3-1/Main.java
@@ -18,8 +18,8 @@ public class Main{
             System.out.print(data[i] + " ");
         }
         System.out.println();
-        for (int i = 0; i < data.length; i++) {
-            for (int j = data.length; j > i; j--) {
+        for (int i = 0; i < data.length - 1; i++) {
+            for (int j = data.length - 1; j > i; j--) {
                 /*
                 * 問3
                 * 以下、配列の添字を入れてソートを完成させなさい
